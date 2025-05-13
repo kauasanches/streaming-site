dbb591b304e72a03f0dae9dd6a2ce4e2e44caa1d
Kauã Sanches <119595660+kauasanches@users.noreply.github.com>
2025-05-13 12:27:55 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 function logar() {
-    
+    var Login = Document.getElementById('login').value;
+    var Senha = Document.getElementById('senha').value;
+
+    if (login=="admin" && senha=="admin") {
+        alert("Sucesso!");
+        location.href="home.html"
+    } else {
+        alert("Usuário ou senha incorretos!");
+    }
 }
